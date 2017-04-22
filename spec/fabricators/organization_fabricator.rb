d52dda96de7987984c6bc384a1f75aa162b17625
Pariwat Chaichomphu <pariwat@Pariwats-MBP.mshome.net>
2017-04-22 11:22:50 +0700

    Add first test for organization model

--- spec/fabricators/organization_fabricator.rb
+++ spec/fabricators/organization_fabricator.rb
@@ -1,6 +1,6 @@
 Fabricator(:organization) do
-  name           "MyString"
-  public_name    "MyString"
+  name            FFaker::Company.name
+  public_name     "#{FFaker::Company.name} #{FFaker::Company.suffix}"
   type           1
   pricing_policy 1
 end
