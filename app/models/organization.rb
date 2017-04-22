63f231fb6d8b2e49efc2931f9dd1e72686370d9d
Pariwat Chaichomphu <pariwat@Pariwats-MBP.mshome.net>
2017-04-22 11:45:22 +0700

    Add value of organization types and pricing policies

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,2 +1,4 @@
 class Organization < ActiveRecord::Base
+  enum org_type: %i(show_room service dealer)
+  enum pricing_policy: %i(flexible fixed prestige)
 end
--- spec/fabricators/organization_fabricator.rb
+++ spec/fabricators/organization_fabricator.rb
@@ -1,6 +1,6 @@
 Fabricator(:organization) do
   name           FFaker::Company.name
   public_name    "#{FFaker::Company.name} #{FFaker::Company.suffix}"
-  org_type       1
-  pricing_policy 1
+  org_type       Organization.org_types.values.sample
+  pricing_policy Organization.pricing_policies.values.sample
 end
