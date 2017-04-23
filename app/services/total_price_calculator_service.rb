733553cbddb1a47d7dfdf203867a0c1a236a8b1d
Pariwat Chaichomphu <pariwat@Pariwats-MBP.mshome.net>
2017-04-24 02:04:32 +0700

    Fix exceptions

--- app/services/total_price_calculator_service.rb
+++ app/services/total_price_calculator_service.rb
@@ -17,7 +17,7 @@ class TotalPriceCalculatorService
                     when 'prestige'
                       @base_price + prestige_margin
                     else
-                      raise Errors::ServicesExceptions::PolicyNotFoundException.new 'Policy not found'
+                      raise Errors::ServicesExceptions::PolicyNotFoundException, 'Policy not found'
                   end
     total_price.floor
   end
